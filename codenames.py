f8d7dddc41921838fde1f1266a2d4bdc1fc38eba
Thomas Dybdahl Ahle <thomas@ahle.dk>
2018-03-01 00:21:46 +0100

    Better, but slightly slower

--- codenames.py
+++ codenames.py
@@ -6,8 +6,10 @@ import math
 
 print('...Loading vectors')
 vectors = np.load('dataset/glove.6B.300d.npy')
-print('...Normalizing')
-vectors /= np.linalg.norm(vectors, axis=1).reshape(-1, 1)
+# Normalizing turns out to be a bad idea, since words that are good clues
+# tends to have longer vectors.
+#print('...Normalizing')
+#vectors /= np.linalg.norm(vectors, axis=1).reshape(-1, 1)
 print('...Loading words')
 word_list = [w.lower().strip() for w in open('dataset/words')]
 print('...Making word to index dict')
@@ -19,10 +21,15 @@ print('Ready!')
 
 log_file = open('log_file', 'w')
 
+# Total number of clues is cnt_rows*cnt_cols.
+# TOtal number of marked words is cnt_agents.
 cnt_rows = 5
 cnt_cols = 5
 cnt_agents = 8
-pos_weight = 2
+
+# Agressiveness in [0, infinity).
+# Higher means more agressive.
+agg = .5
 
 def word_to_vector(word):
     return vectors[word_to_index[word]]
@@ -30,14 +37,6 @@ def word_to_vector(word):
 def most_similar_to_given(clue, choices):
     clue_vector = word_to_vector(clue)
     return max(choices, key=lambda w: word_to_vector(w) @ clue_vector)
-
-def most_similar(pos, pm, negs, nm):
-    gap = pos_weight * np.array(pm).min(axis=0)
-    if negs:
-        gap -= nm
-    i = gap.argmax()
-    s = gap.max()
-    return word_list[i], s
 
 def print_words(words):
     longest = max(map(len, words))
@@ -48,25 +47,27 @@ def print_words(words):
         print()
     print()
 
-def find_clue(words, my_words):
-    my_words = list(my_words)
-    best_word, best_score, best_k, best_g = None, -1, 0, ()
+def find_clue(words, my_words, black_list):
+    print('Thinking', end='', flush=True)
+
     negs = [w for w in words if w not in my_words]
-    nm = np.array([vectors @ word_to_vector(word) for word in negs]).max(axis=0)
-    pm = np.array([vectors @ word_to_vector(word) for word in my_words])
-    for k in range(len(my_words), 0, -1):
-        for ids in itertools.combinations(range(len(my_words)), k):
-            group = [my_words[i] for i in ids]
-            word, score = most_similar(group, [pm[i] for i in ids], negs, nm)
-            score *= (k**.5-1)
-            # Punish weird words
-            score /= math.log(word_to_index[word]+2) / 10
-            if score > best_score:
-                best_word, best_score, best_k, best_g = word, score, k, group
-                print('Thinking {} ... {:.3f}'.format(k, score), word_to_index[word])
-                scores = [word_to_vector(word) @ word_to_vector(g) for g in group]
-                print(word, score, group, scores, file=log_file, flush=True)
-    return best_word, best_k, best_score, best_g
+    nm = (vectors @ np.array([word_to_vector(word) for word in negs]).T).max(axis=1)
+    pm = vectors @ np.array([word_to_vector(word) for word in my_words]).T
+
+    best_clue, best_score, best_k, best_g = None, -1, 0, ()
+    for step, (clue, lower_bound, scores) in enumerate(zip(word_list, nm, pm)):
+        if step % 20000 == 0:
+            print('.', end='', flush=True)
+        if max(scores)-lower_bound <= lower_bound or clue in black_list:
+            continue
+        ss = sorted((s,i) for i,s in enumerate(scores - lower_bound))
+        real_score, j = max((s*((len(ss) - j)**agg-.99), j) for j,(s,_) in enumerate(ss))
+        if real_score > best_score:
+            group = [my_words[i] for _, i in ss[j:]]
+            best_clue, best_score, best_k, best_g = clue, real_score, len(group), group
+    print()
+
+    return best_clue, best_score, best_g
 
 def read_picks(words, my_words, cnt):
     picks = []
@@ -96,13 +97,22 @@ def read_clue():
 def play_spymaster():
     words = random.sample(codenames, cnt_rows*cnt_cols)
     my_words = set(random.sample(words, cnt_agents))
+    used_clues = set(my_words)
     while my_words:
         print_words(words)
-        word, cnt, score, group = find_clue(words, my_words)
+
+        clue, score, group = find_clue(words, list(my_words), used_clues)
+        # Print the clue to the log_file for "debugging" purposes
+        group_scores = np.array([word_to_vector(w) for w in group]) @ word_to_vector(clue)
+        print(clue, group, group_scores, file=log_file, flush=True)
+        # Save the clue, so we don't use it again
+        used_clues.add(clue)
+
         print()
-        print('Clue: {} {}! (Score: {})'.format(word, cnt, score))
+        print('Clue: "{} {}" (certainty {:.2f}, remaining words {})'
+                .format(clue, len(group), score, len(my_words)))
         print()
-        for pick in read_picks(words, my_words, cnt):
+        for pick in read_picks(words, my_words, len(group)):
             words[words.index(pick)] = '---'
             if pick in my_words:
                 my_words.remove(pick)
--- get_glove.sh
+++ get_glove.sh
@@ -4,4 +4,4 @@ wget --no-check-certificate http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip glove.6B.300d.txt
 rm globe.6B.zip
 cd ..
-python convert.py
+python3 convert.py
