7084e5064555c2d85e0dc87dd2c7e9c45165f58c
Thomas Dybdahl Ahle <thomas@ahle.dk>
2018-03-01 01:13:22 +0100

    Fix from changchuming

--- get_glove.sh
+++ get_glove.sh
@@ -2,6 +2,6 @@ mkdir dataset
 cd dataset
 wget --no-check-certificate http://nlp.stanford.edu/data/glove.6B.zip
 unzip glove.6B.zip glove.6B.300d.txt
-rm globe.6B.zip
+rm glove.6B.zip
 cd ..
 python3 convert.py
